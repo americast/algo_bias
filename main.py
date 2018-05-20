a15161af036bf40d46b910937fdf2d02e5abd4ad
americast <sayan.sinha@iitkgp.ac.in>
2018-05-20 18:25:24 +0530

    Extract year from date

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import torch
 import numpy as np
 import pandas as pd
+import sys
+
+BATCH_SIZE = 9
+
+def get_yr(x):
+	return x.split('/')[-1]
+
+
+def process(req):
+	req_new = req.loc[:, ['DateOfBirth']]
+	# print(req_new)
+	req_new = req_new.applymap(get_yr)
+	return req_new
 
 # Read the data
 df = pd.read_csv("data/out.csv")
@@ -9,4 +22,10 @@ cols = np.array([3,7,8,9,10,13,14,15,16,17,18,22])
 cols += 1
 df = df[df.columns[cols]]
 
-print(df)
+for i in xrange((df.shape[0])/BATCH_SIZE):
+	curr = i * BATCH_SIZE
+	req = df.loc[curr:curr+BATCH_SIZE, :]
+	final_tensor = process(req)
+	print(final_tensor["DateOfBirth"])
+
+# print(df.shape[0])
