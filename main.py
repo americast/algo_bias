5870e6d9df1596243a135292494a3dc263eb0b58
americast <sayan.sinha@iitkgp.ac.in>
2018-05-22 11:19:48 +0530

    Age extraction done

--- main.py
+++ main.py
@@ -5,15 +5,29 @@ import sys
 
 BATCH_SIZE = 9
 
-def get_yr(x):
-	return x.split('/')[-1]
+def get_db_yr(x):
+	return int(x.split('/')[-1])
+
+def get_sc_yr(x):
+	return int(x.split('/')[-1].split(' ')[0]) + 100
 
 
-def process(req):
-	req_new = req.loc[:, ['DateOfBirth']]
-	# print(req_new)
-	req_new = req_new.applymap(get_yr)
+def process_sc(req):
+	req_new = req.loc[:,['Screening_Date']]
+	req_new = req_new.applymap(get_sc_yr)
 	return req_new
+
+def process_dob(req):
+	req_new = req.loc[:, ['DateOfBirth']]
+	req_new = req_new.applymap(get_db_yr)
+	return req_new
+
+def get_age(req):
+	req["Age"]=req.loc[:,['Screening_Date']].sub(req['DateOfBirth'], axis=0)["Screening_Date"]
+	# print req["Age"]
+	return req
+	# pass
+
 
 # Read the data
 df = pd.read_csv("data/out.csv")
@@ -25,7 +39,11 @@ df = df[df.columns[cols]]
 for i in xrange((df.shape[0])/BATCH_SIZE):
 	curr = i * BATCH_SIZE
 	req = df.loc[curr:curr+BATCH_SIZE, :]
-	final_tensor = process(req)
-	print(final_tensor["DateOfBirth"])
+	req["DateOfBirth"] = process_dob(req)["DateOfBirth"]
+	req["Screening_Date"] = process_sc(req)["Screening_Date"]
+	# print(req["Screening_Date"])
+	req = get_age(req)
+	print(req)
+	# print(final_tensor[:,["DateOfBirth","Screening_Date"]])
 
 # print(df.shape[0])
