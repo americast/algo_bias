3eb58fb7d7290857ddbc762118bd4513aba6b60f
americast <sayan.sinha@iitkgp.ac.in>
2018-05-22 12:27:09 +0530

    obtain final tensors

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pandas as pd
 import sys
 
 BATCH_SIZE = 9
+ACCESS_PARAM = "Scale_ID"
+
+count = 0
 
 def get_db_yr(x):
 	return int(x.split('/')[-1])
@@ -28,22 +31,44 @@ def get_age(req):
 	return req
 	# pass
 
+def create_tensor(req):
+	to_drop=[]
+	global count
+	# print( req)
+	for i in xrange(BATCH_SIZE):
+		if not req.at[count,ACCESS_PARAM] == 7:
+			to_drop.append(i)
+		count+=1
+	print(to_drop)
+	req = req.drop(req.index[to_drop])
+	array_now = req.loc[:,["Agency_Text","Sex_Code_Text","Ethnic_Code_Text","Language","LegalStatus","CustodyStatus","MaritalStatus","Age"]].values
+	array_now = torch.transpose(torch.tensor(array_now), 0, 1)
+	# print(array_now)
+	# print("rockstud")
+
+	result_now = req.loc[:,["RawScore"]].values
+	result_now =torch.transpose(torch.tensor(result_now), 0, 1)
+	return array_now, result_now
+
 
 # Read the data
 df = pd.read_csv("data/out.csv")
 
-cols = np.array([3,7,8,9,10,13,14,15,16,17,18,22])
+cols = np.array([3,7,8,9,10,13,14,15,16,17,18,20,22])
 cols += 1
 df = df[df.columns[cols]]
 
 for i in xrange((df.shape[0])/BATCH_SIZE):
 	curr = i * BATCH_SIZE
-	req = df.loc[curr:curr+BATCH_SIZE, :]
+	req = df.loc[curr:curr+BATCH_SIZE-1, :]
 	req["DateOfBirth"] = process_dob(req)["DateOfBirth"]
 	req["Screening_Date"] = process_sc(req)["Screening_Date"]
 	# print(req["Screening_Date"])
 	req = get_age(req)
-	print(req)
+	# print(req)
+	final_tensor, result = create_tensor(req)
+	print(final_tensor,"\n", result)
+	print(":rockstud:")
 	# print(final_tensor[:,["DateOfBirth","Screening_Date"]])
 
 # print(df.shape[0])
