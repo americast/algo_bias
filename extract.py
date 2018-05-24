65c17a0734f9e9b591254df7396df35b585b7f2c
americast <sayan.sinha@iitkgp.ac.in>
2018-05-24 10:28:43 +0530

    New model

--- extract.py
+++ extract.py
@@ -2,11 +2,34 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+def get_db_yr(x):
+	return int(x.split('/')[-1])
+
+def get_sc_yr(x):
+	return int(x.split('/')[-1].split(' ')[0]) + 100
+
+
+def process_sc(req):
+	req_new = req.loc[:,['Screening_Date']]
+	req_new = req_new.applymap(get_sc_yr)
+	return req_new
+
+def process_dob(req):
+	req_new = req.loc[:, ['DateOfBirth']]
+	req_new = req_new.applymap(get_db_yr)
+	return req_new
+
+def get_age(req):
+	req["Age"]=req.loc[:,['Screening_Date']].sub(req['DateOfBirth'], axis=0)["Screening_Date"]
+	# print req["Age"]
+	return req
+	# pass
+
 df = pd.read_csv("data/raw.csv")
+# print(df["RawScore"].max())
 df = df[df.DisplayText != 'Risk of Failure to Appear']
 df = df[df.DisplayText != 'Risk of Recidivism']
 cols = df.columns.values
-
 
 req_cols =[cols[i] for i in (3,7,8,13,14,15,16)]
 
@@ -17,6 +40,12 @@ for col in req_cols:
 		df[col] = df[col] .replace([each_row], i)
 		i+=1
 		
+df["DateOfBirth"] = process_dob(df)["DateOfBirth"]
+df["Screening_Date"] = process_sc(df)["Screening_Date"]
+df = get_age(df)
+
+df["RawScore"] += np.abs(df["RawScore"].min() )+ 1.0
+
 train, test = train_test_split(df, test_size=(1.0/6.0))
 
 df.to_csv("data/out.csv")
--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import argparse
 import numpy as np
 import pandas as pd
 import sys
+import os
 
 BATCH_SIZE = 8192
 ACCESS_PARAM = "Scale_ID"
-EPOCHS = 10
+EPOCHS = 100000
 LEARNING_RATE = 0.01
 # SGD_MOMENTUM = 0.5
 
@@ -17,42 +18,39 @@ class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # self.conv2_drop = nn.Dropout2d()
-        self.fc1 = torch.nn.Linear(8, 4)
-        self.fc2 = torch.nn.Linear(4, 1)
+        self.fc1 = torch.nn.Linear(8, 16)
+        self.dense1_bn = torch.nn.BatchNorm1d(16)
+        self.fc2 = torch.nn.Linear(16, 32)
+        self.dense2_bn = torch.nn.BatchNorm1d(32)
+        self.fc3 = torch.nn.Linear(32, 32)
+        self.dense3_bn = torch.nn.BatchNorm1d(32)
+        self.fc4 = torch.nn.Linear(32, 16)
+        self.dense4_bn = torch.nn.BatchNorm1d(16)
+        self.fc5 = torch.nn.Linear(16, 8)
+        self.dense5_bn = torch.nn.BatchNorm1d(8)
+        self.fc6 = torch.nn.Linear(8, 1)
 
     def forward(self, x):
         # x = F.relu(F.max_pool2d(self.conv1(x), 2))
         # x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         # x = x.view(-1, 320)
-        x = torch.nn.functional.relu(self.fc1(x))
-        x = torch.nn.functional.relu(self.fc2(x))
+        x = torch.nn.functional.relu(self.dense1_bn(self.fc1(x)))
+        x = torch.nn.functional.dropout(x)
+        x = torch.nn.functional.relu(self.dense2_bn(self.fc2(x)))
+        x = torch.nn.functional.dropout(x)
+        x = torch.nn.functional.relu(self.dense3_bn(self.fc3(x)))
+        x = torch.nn.functional.dropout(x)
+        x = torch.nn.functional.relu(self.dense4_bn(self.fc4(x)))
+        x = torch.nn.functional.dropout(x)
+        x = torch.nn.functional.relu(self.dense5_bn(self.fc5(x)))
+        x = torch.nn.functional.dropout(x)
+        x = torch.nn.functional.relu(self.fc6(x))
         # x = F.dropout(x, training=self.training)
         # loss = torch.nn.MSELoss()
         # output = loss(out_tensor, result)
         # return F.log_softmax(x, dim=1)
         return x
 
-def get_db_yr(x):
-	return int(x.split('/')[-1])
-
-def get_sc_yr(x):
-	return int(x.split('/')[-1].split(' ')[0]) + 100
-
-
-def process_sc(req):
-	req_new = req.loc[:,['Screening_Date']]
-	req_new = req_new.applymap(get_sc_yr)
-	return req_new
-
-def process_dob(req):
-	req_new = req.loc[:, ['DateOfBirth']]
-	req_new = req_new.applymap(get_db_yr)
-	return req_new
-
-def get_age(req):
-	req["Age"]=req.loc[:,['Screening_Date']].sub(req['DateOfBirth'], axis=0)["Screening_Date"]
-	# print req["Age"]
-	return req
 	# pass
 
 def create_tensor(req):
@@ -78,7 +76,7 @@ def create_tensor(req):
 
 
 # Read the data
-df = pd.read_csv("data/out.csv")
+df = pd.read_csv("data/out-train.csv")
 
 cols = np.array([3,7,8,9,10,13,14,15,16,17,18,20,22])
 cols += 1
@@ -117,14 +115,9 @@ for j in xrange(EPOCHS):
 	losses=0.0
 	global count
 	count = 0
-	for i in xrange((df.shape[0])/BATCH_SIZE):
+	for i in xrange(((df.shape[0])/BATCH_SIZE) +1):
 		curr = i * BATCH_SIZE
 		req = df.loc[curr:curr+BATCH_SIZE-1, :]
-		req["DateOfBirth"] = process_dob(req)["DateOfBirth"]
-		req["Screening_Date"] = process_sc(req)["Screening_Date"]
-		# print(req["Screening_Date"])
-		req = get_age(req)
-		# print(req)
 		final_tensor, result = create_tensor(req)
 		# print(final_tensor,"\n", result)
 
@@ -139,10 +132,13 @@ for j in xrange(EPOCHS):
 		losses+=loss_here.data[0]
 		# print(final_tensor[:,["DateOfBirth","Screening_Date"]])
 		count+=BATCH_SIZE
+		if count>df.shape[0]:
+			count=df.shape[0]
 		print("Batches done: "+str(count)+"/"+str(df.shape[0]),end="\r")
 	losses/=count
 	print("\nIteration "+str(j+1)+"/"+str(EPOCHS)+" done")
 	print("\nLoss: "+ str(losses))
+	os.system("mkdir -p checkpoints")
 	torch.save(model.state_dict(), "checkpoints/"+str(j))
 
 	# print(df.shape[0])
