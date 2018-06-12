f1564952d4dbf7cd83353988b891b877bcdb43dd
americast <sayan.sinha@iitkgp.ac.in>
2018-06-12 18:08:36 +0530

    binary

--- kmain.py
+++ kmain.py
@@ -20,34 +20,34 @@ model.add(Activation('selu'))
 
 model.add(Dense(64))
 model.add(BatchNormalization())
-model.add(Activation('selu'))
-model.add(Dropout(0.4))
+model.add(Activation('sigmoid'))
+# model.add(Dropout(0.4))
 
 
 model.add(Dense(128))
 model.add(BatchNormalization())
-model.add(Activation('selu'))
-model.add(Dropout(0.4))
+model.add(Activation('sigmoid'))
+# model.add(Dropout(0.4))
 
 model.add(Dense(64))
 model.add(BatchNormalization())
-model.add(Activation('selu'))
-model.add(Dropout(0.4))
+model.add(Activation('sigmoid'))
+# model.add(Dropout(0.4))
 
 
 model.add(Dense(32))
 model.add(BatchNormalization())
 model.add(Activation('selu'))
-model.add(Dropout(0.4))
+# model.add(Dropout(0.4))
 
 
 model.add(Dense(8))
 model.add(BatchNormalization())
 model.add(Activation('selu'))
-model.add(Dropout(0.4))
+# model.add(Dropout(0.4))
 
-model.add(Dense(2))
-model.add(Activation('softmax'))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
 print(model.summary())
 
 print("Would you like to restore a previously saved model? (y/n)")
@@ -88,15 +88,15 @@ x_train = df.loc[:,['Age', 'Agency_Text1', 'Agency_Text2',
    'MaritalStatus6', 'MaritalStatus7']].values
 
 y_train = df["DecileScore"].values
-y_train = keras.utils.np_utils.to_categorical(y_train)
+# y_train = keras.utils.np_utils.to_categorical(y_train)
 
 model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
+              loss='binary_crossentropy',
               metrics=['accuracy'])
 
 checkpointer = ModelCheckpoint(monitor='acc', filepath="checkpoints/model.h5", verbose=True,
                                    save_best_only = True)
-earlystopping = EarlyStopping(monitor='acc', min_delta=1e-6, patience=100, verbose=True)
+earlystopping = EarlyStopping(monitor='acc', min_delta=1e-6, patience=20, verbose=True)
 
 if train_flag:
 	model.fit(x_train, y_train,
