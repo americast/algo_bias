decfbcc7060d470f5f42af35797437008b927816
Rakesh Bal <rakeshbal99@gmail.com>
2018-06-17 21:09:00 +0530

    Added people.csv and extractor

--- extract.py
+++ extract.py
@@ -2,68 +2,45 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def get_db_yr(x):
-	return int(x.split('/')[-1])
+df = pd.read_csv("data/people.csv")
 
-def get_sc_yr(x):
-	return int(x.split('/')[-1].split(' ')[0]) + 100
-
-
-def process_sc(req):
-	req_new = req.loc[:,['Screening_Date']]
-	req_new = req_new.applymap(get_sc_yr)
-	return req_new
-
-def process_dob(req):
-	req_new = req.loc[:, ['DateOfBirth']]
-	req_new = req_new.applymap(get_db_yr)
-	return req_new
-
-def get_age(req):
-	req["Age"]=req.loc[:,['Screening_Date']].sub(req['DateOfBirth'], axis=0)["Screening_Date"]
-	return req
-
-df = pd.read_csv("data/raw.csv")
-# print(df["RawScore"].max())
-# df = df[df.DisplayText != 'Risk of Failure to Appear']
-# df = df[df.DisplayText != 'Risk of Recidivism']
-# df = df[df.DisplayText != 'Risk of Violence']
 cols = df.columns.values
 
-df["DateOfBirth"] = process_dob(df)["DateOfBirth"]
-df["Screening_Date"] = process_sc(df)["Screening_Date"]
-df = get_age(df)
-
-req_cols =[cols[i] for i in (3,7,8,13,14,15,16)]
+req_cols =[cols[i] for i in (4,5,8,9,10,11,24)]
 
 for col in req_cols:
 	unique_rows = df[col].unique()
 	i = 1
 	for each_row in unique_rows:
-		df[col + str(i)] = (df[col] == each_row) *1
+		df[col + str(i)] = (df[col] == each_row) * 1
 		i+=1
+
+col = 'c_charge_desc'
+i = 1
+for each_row in df[col].unique():
+	df[col] = df[col] .replace([each_row], i)
+	i+=1
 
 lower_threshold = 4.0
 middle_threshold = 7.0
 
-df1 = (df["DecileScore"] > lower_threshold) * 1
-# df2 = (df["DecileScore"] > middle_threshold) * 1 
+df1 = (df["decile_score"] > lower_threshold) * 1
+# df2 = (df["decile_score"] > middle_threshold) * 1 
 
 # df_add = df1.add(df2, fill_value=0)
 
-df["DecileScore"] = df1
+df["decile_score"] = df1
 
-df = df.drop(columns=["Person_ID", "AssessmentID", "Case_ID", "Agency_Text", "LastName", "FirstName", "MiddleName", "Sex_Code_Text", "Ethnic_Code_Text", "DateOfBirth", "ScaleSet_ID", "ScaleSet", "AssessmentReason", "Language", "LegalStatus", "CustodyStatus", "MaritalStatus", "Screening_Date", "RecSupervisionLevel", "RecSupervisionLevelText", "Scale_ID", "DisplayText", "RawScore", "ScoreText", "AssessmentType", "IsCompleted", "IsDeleted"])
-df = df.drop(df[df['DecileScore'] == 0].sample(frac=0.5).index)
+cols = np.delete(cols,[7,13,16,25])
+
+df = df.drop(columns = cols)
+
 train, test = train_test_split(df, test_size=(1.0/6.0))
 
-df.to_csv("data/out.csv")
-train.to_csv("data/out-train.csv")
-test.to_csv("data/out-test.csv") 
+df.to_csv("data/out.csv", index=False)
+train.to_csv("data/out-train.csv", index=False)
+test.to_csv("data/out-test.csv",  index=False) 
 
 print(train.shape)
 print(test.shape)
 print(df.shape)
-print(df["DecileScore"].value_counts())
-print(train["DecileScore"].value_counts())
-print(test["DecileScore"].value_counts())
