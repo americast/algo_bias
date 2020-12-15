2cd839acf29ec2450867a27b89b3fff35dc95e18
Anshul Sharma <thevoxium@gmail.com>
2020-12-15 23:52:48 +0530

    reversed to previous commit

--- lrp_matrix.py
+++ lrp_matrix.py
@@ -7,29 +7,25 @@ import pudb
 # req_cols =[cols[i] for i in (4,5,7,8,9,10,11,16,24)]
 
 # array = [1,2,6,66,3,12,11,12,39,14]
-layer_num = [2,6,10,14,18,22,26,30,34,38,42, 44]
 array = [0,1,3,9,75,78,90,101,113,152,166]
-g = np.zeros((10,13))
+g = np.zeros((10,8))
 f = np.zeros((10,1))
 count = 0
-k=1
-for x in layer_num:
-	temp = np.load("out_lrp_"+str(x)+".npy")
-	print(temp.shape)
+for x in range(1,9):
+	temp = np.load("out_"+str(x)+"_lrp.npy")
 	temp = np.sum(temp,axis=0,keepdims=True)
-	print(temp.shape)
 	temp = temp.transpose()
-	print(temp.shape)
+
+
 	for i in range(0,10):
 		for j in range(array[i],array[i+1]):
-			if (j>107):
-				break
 			f[i] = f[i] + temp[j] 
-	k=k+1
+
 	print(np.min(f))	
 	f = f-np.min(f)
 	f = f/np.max(f)
-	print("X: ", x)
-	g[0:,[k-1]] = f
-	name="lrp_matrix"+str(x)
-	np.save(name, g)
+	g[0:,[x-1]] = f
+
+np.save("lrp_matrix", g)
+df = pd.DataFrame(g)
+df.to_csv("lrp_matrix.csv", index=False)
--- plot.py
+++ plot.py
@@ -1,11 +1,9 @@
 import numpy as np
 import cv2
 
-layer_num = [2,6,10,14,18,22,26,30,34,38,42, 44]
-
-for x in layer_num:
-  a=np.load("lrp_matrix"+str(x)+".npy")
-  a=np.repeat(a, 13, axis=0)
-  a=np.repeat(a, 13, axis=1)
-  name="pic_"+str(x)+".png"
-  cv2.imwrite(name, a*255)
+a = np.load("lrp_matrix.npy")
+a = np.repeat(a, 8, axis = 0)
+a = np.repeat(a, 8, axis = 1)
+cv2.imwrite("pic_1.png", a*255)
+# cv2.imshow("a", a)
+# cv2.waitKey(0)
